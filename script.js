c44ba6a077815008f84b3a8f23ccfd38e82677fa
aminariaz1 <riazamina990@gmail.com>
2024-09-29 00:10:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+    const fruits=["apple" , "banana" , "peach"];
+    fruits.push("orangre");
+
+    const bikes=[ "honda", 70 ,"metro"]
+    bikes[1]="civic";
+
+   const mobiles=["vivo" , "realme" , "iPhone"];
+    console.log(mobiles);
+
+    const cars=["BMW" , "mercedes" , "lamborghini"];
+    cars.push("daala")
+    cars[1]="civic";
+
+   const languages=["english" , "urdu" , "punjabi"];
+   languages[0] = "spanish";
+   languages.push("persian");
+   console.log(languages); 
+
+    const laptops=[ "hp", "asus","dell" ];
+    laptops.push("daala")
+    laptops[1]="civic";
+    console.log(laptops);
+
+    const sports = ["cricket", "football", "tennis"];
+   sports[2] = "basketball";
+   sports.push("hockey");
+   console.log(sports);
+
+   const colors = ["red", "white", "green"];
+   colors[2] = "aqua";
+   colors.push("blue");
+   console.log(colors);
+
+
+   const animals = ["dog", "cat", "tigers"];
+   animals[1] = "lions";
+   animals.push("sapparow");
+   console.log(animals);
+
+   const novels=["ab-e-hayat" , "hasil" , "rooh-e-yaram"];
+   novels.push("peer-e-kamil");
+    console.log(novels);
+
+   
